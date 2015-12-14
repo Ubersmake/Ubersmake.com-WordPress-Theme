93f3d6317aa4a1ae9da5237ec501fdcf7b319dde
Paul Morales <pomorales@gmail.com>
2015-12-14 00:18:24 -0700

    Custom headers.

--- functions.php
+++ functions.php
@@ -5,3 +5,39 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+/* Modified from pilcrow/inc/custom-header.php */
+remove_action( 'after_setup_theme', 'pilcrow_custom_header_setup' );
+function ubersmake_custom_header_setup() {
+  $args = array(
+    'default-image'          => get_stylesheet_directory_uri() . '/images/headers/bazaar.jpg',
+    'default-text-color'     => '000',
+    'width'                  => apply_filters( 'pilcrow_header_image_width', 990 ),
+    'height'                 => apply_filters( 'pilcrow_header_image_height', 257 ),
+    'wp-head-callback'       => 'pilcrow_header_style',
+    'admin-head-callback'    => 'pilcrow_admin_header_style',
+    'admin-preview-callback' => 'pilcrow_admin_header_image',
+  );
+
+  $options = pilcrow_get_theme_options();
+  if ( in_array( $options['theme_layout'], array( 'content-sidebar', 'sidebar-content' ) ) ) {
+    $args['width']  = apply_filters( 'pilcrow_header_image_width', 770 );
+    $args['height'] = apply_filters( 'pilcrow_header_image_height', 200 );
+  }
+
+  add_theme_support( 'custom-header', apply_filters( 'pilcrow_custom_header_args', $args ) );
+
+  // We'll be using post thumbnails for custom header images on posts and pages.
+  // Larger images will be auto-cropped to fit, smaller ones will be ignored. See header.php.
+  set_post_thumbnail_size( get_custom_header()->width, get_custom_header()->height, true );
+
+  // Default custom headers packaged with the theme. %s is a placeholder for the theme template directory URI.
+  register_default_headers( array(
+    'pattern' => array(
+      'url'           => get_stylesheet_directory_uri() . '/images/headers/bazaar.jpg',
+      'thumbnail_url' => get_stylesheet_directory_uri() . '/images/headers/bazaar-thumbnail.jpg',
+      'description'   => _x( 'Pattern', 'Header image description', 'ubersmake' )
+    ),
+  ) );
+}
+add_action( 'after_setup_theme', 'ubersmake_custom_header_setup' );
