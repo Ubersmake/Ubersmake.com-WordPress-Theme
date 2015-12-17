3bd5914ea44d065a62978162601a6bdbf4baa10a
Paul Morales <pomorales@gmail.com>
2015-12-16 22:31:53 -0800

    Remove unused code.

--- loop.php
+++ loop.php
@@ -67,33 +67,6 @@ if ( false === $format )
         <?php if (has_post_thumbnail( $post->ID ) ): ?>
             style="background-size: cover; background-position: center; background-image: url('<?php the_post_thumbnail_url( 'medium' ); ?>');"
         <?php endif; ?>>
-        <!-- No entry meta(data)
-        <div class="entry-meta">
-
-            <?php
-                if ( 'standard' == $format ) :
-                    if ( is_multi_author() ) :
-                        printf( __( '<span class="by-author"><span class="sep">by</span> <span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span> | </span>', 'pilcrow' ),
-                            esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-                            esc_attr( sprintf( __( 'View all posts by %s', 'pilcrow' ), get_the_author_meta( 'display_name' ) ) ),
-                            esc_attr( get_the_author_meta( 'display_name' ) )
-                        );
-                    endif;
-
-                    the_date(); ?> &middot; <?php the_time();
-                else :
-            ?>
-            <span class="entry-format">
-                <a class="entry-format-link" href="<?php echo esc_url( get_post_format_link( get_post_format() ) ); ?>" title="<?php echo esc_attr( sprintf( __( 'All %s posts', 'pilcrow' ), get_post_format_string( get_post_format() ) ) ); ?>"><?php echo get_post_format_string( get_post_format() ); ?></a>
-            </span>
-            <?php
-                endif;
-
-                edit_post_link( __( 'Edit', 'pilcrow' ), '<span class="edit-link"> | ', '</span>' );
-            ?>
-        </div>
-        -->
-        <!-- .entry-meta -->
 
         <?php
             if ( 'link' != $format ) :
@@ -126,29 +99,6 @@ if ( false === $format )
             <?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'pilcrow' ), 'after' => '</div>' ) ); ?>
         </div><!-- .entry-content -->
         <?php endif; // is_search ?>
-
-        <!--  No entry links.
-        <div class="entry-links">
-            <p class="comment-number"><?php comments_popup_link( __( 'Leave a Comment', 'pilcrow' ), __( '1 Comment', 'pilcrow' ), __( '% Comments', 'pilcrow' ) ); ?></p>
-
-            <?php
-                if ( 'standard' != $format ) :
-                    if ( is_multi_author() ) :
-                        printf( __( '<span class="by-author"><span class="sep">by</span> <span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s" rel="author">%3$s</a></span> | </span>', 'pilcrow' ),
-                            esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
-                            esc_attr( sprintf( __( 'View all posts by %s', 'pilcrow' ), get_the_author_meta( 'display_name' ) ) ),
-                            esc_attr( get_the_author_meta( 'display_name' ) )
-                        );
-                    endif;
-            ?>
-                <a href="<?php the_permalink(); ?>"><?php the_time( get_option( 'date_format' ) ); ?></a> &middot; <?php the_time(); ?>
-            <?php else : ?>
-                <p class="entry-categories tagged"><?php printf( __( 'Filed under %s', 'pilcrow' ), get_the_category_list( ', ' ) ); ?></p>
-                <p class="entry-tags tagged"><?php the_tags( __( 'Tagged as', 'pilcrow' ).' ', ', ', '<br />' ); ?></p>
-            <?php endif; ?>
-        </div>
-        -->
-        <!-- .entry-links -->
     </div>
 </div><!-- #post-## -->
 
