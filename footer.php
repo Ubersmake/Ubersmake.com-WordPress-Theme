cabcbfdcdafed8cc18d9053a3e2e27d5cab926b1
Paul Morales <pomorales@gmail.com>
2015-12-18 02:27:44 -0800

    Remove footer.

--- footer.php
+++ footer.php
@@ -11,29 +11,6 @@
 ?>
 
 		</div><!-- #content-box -->
-
-		<div id="footer" role="contentinfo">
-			<div id="colophon">
-
-				<?php
-					/* A sidebar in the footer? Yep. You can can customize
-					 * your footer with two columns of widgets.
-					 */
-					get_sidebar( 'footer' );
-				?>
-
-				<div id="site-info">
-					<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a> &middot; <?php bloginfo( 'description' ); ?>
-				</div><!-- #site-info -->
-
-				<div id="site-generator">
-					<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'pilcrow' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'pilcrow' ), 'WordPress' ); ?></a>
-					&middot;
-					<?php printf( __( 'Theme: %1$s by %2$s.', 'pilcrow' ), 'Pilcrow', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
-				</div><!-- #site-generator -->
-
-			</div><!-- #colophon -->
-		</div><!-- #footer -->
 	</div><!-- #page .blog -->
 </div><!-- #container -->
 
